a309747287ac907eec4c5c3806f9e59847504013
Enzo Maldinni <enzo.maldinni@hotmail.com>
2026-01-20 12:25:33 -0300

    [UPDATE] Caminhos do config alinhados com a arquitetura

--- src/automation/config/settings.py
+++ src/automation/config/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv # Recebe as variáveis de ambiente
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 load_dotenv(BASE_DIR / "keys.env")
 
 #print(teste)
