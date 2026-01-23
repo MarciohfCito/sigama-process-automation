cb2f691a962ae3aec808d3a4cfadb938cabebd9a
Marcio <mchcito@gmail.com>
2026-01-23 12:50:11 -0300

    Alteração no settings.py, criação da função resource_path e mudança no basepath

--- src/automation/config/settings.py
+++ src/automation/config/settings.py
@@ -1,9 +1,17 @@
 import os
+import sys
 from pathlib import Path
 from dotenv import load_dotenv # Recebe as variáveis de ambiente
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 load_dotenv(BASE_DIR / "keys.env")
+
+def resource_path(rel_path):
+    base_path = getattr(sys, '_MEIPASS', Path(__file__).resolve().parents[2])
+    return Path(base_path) / rel_path
+
+env_path = resource_path("keys.env")
+load_dotenv(env_path)
 
 #print(teste)
 #print(BASE_DIR)
@@ -22,7 +30,17 @@ load_dotenv(BASE_DIR / "keys.env")
 
 #as configs de app receberão os itens do dicionario para acertar os parametros
 
-BASEPATH = Path(os.getenv("SIGAMA_BASEPATH")) #caminho padrão do servidor para acesso e manipulação das pastas e arquivos do projeto, criar e alterar o arquivo .env para conter o SIGAMA_BASEPATH(caminho)
+#BASEPATH = Path(os.getenv("SIGAMA_BASEPATH")) #caminho padrão do servidor para acesso e manipulação das pastas e arquivos do projeto, criar e alterar o arquivo .env para conter o SIGAMA_BASEPATH(caminho)
+basepath_env = os.getenv("SIGAMA_BASEPATH")
+
+if not basepath_env:
+    raise RuntimeError(
+        "Variável de ambiente SIGAMA_BASEPATH não definida. "
+        "Verifique o arquivo keys.env"
+    )
+
+BASEPATH = Path(basepath_env)
+
 LOCAL_BASEPATH = Path(os.getenv("LOCAL_BASEPATH"))
 IMAGE_BASEPATH = Path(os.getenv("IMAGES_FOLDER"))
 
